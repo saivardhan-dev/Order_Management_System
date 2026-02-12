d2465ad3a7fb3c57dd96acfab0a9b5753b34ee8f
saivardhan <saivardhanreddynarra@Sais-MacBook-Air-4.local>
2026-02-12 10:26:18 -0600

    Order Management System Application <-> Microservices

--- User_Service/src/main/java/com/microservices/user_service/model/UserModel.java
+++ User_Service/src/main/java/com/microservices/user_service/model/UserModel.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class UserModel {
 
     @Id
-    private String id; // userId
+    private String id;
     private String name;
     private List<Address> addresses;
 
